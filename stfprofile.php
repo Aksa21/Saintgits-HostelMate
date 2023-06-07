7d6799b1ba382d4b0479698c3705c9f810f3b67e
Anjali Mathew <anjalimathew468@gmail.com>
2023-06-07 09:36:31 +0530

    after photo

--- stfprofile.php
+++ stfprofile.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// Retrieve the std_id from the session variable
+// Retrieve the stf_id from the session variable
 $stf_id = $_SESSION['stf_id'];
 
 // Assuming you have established a database connection
@@ -25,6 +25,7 @@ if ($result->num_rows == 1) {
     $mob = $row['mob'];
     $dob = $row['dob'];
     $address = $row['address'];
+    $image1 = $row['image'];
 } else {
     // Name not found, handle the error
     $name = "Unknown";
@@ -33,12 +34,12 @@ if ($result->num_rows == 1) {
     $mob = "";
     $dob = "";
     $address = "";
+    $image1 = '';
 }
 
 // Close the database connection
 $conn->close();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -62,9 +63,13 @@ $conn->close();
             <h3 class="stdtag">Profile</h3>
         </div>
     </div>
-    <div class="row no-gutters">
+    <div class="row ">
         <div class="col-3">
-            <img src="icon1.png" class="img-fluid1">
+            <?php if (!empty($image1)) { ?>
+                <img src="<?php echo $image1; ?>" class="img-fluid1">
+            <?php }else{?>
+                <img src="icon1.png" class="img-fluid1">
+            <?php } ?>
             <h5 style="padding-left:27%; color: rgb(32, 32, 50); padding-top: 1%; font-weight: bold; background: whitesmoke; " ><?php echo $name; ?></h5>
 
             <form action="staffhome.php">
@@ -91,39 +96,55 @@ $conn->close();
         </div>
             
         <div class="col-9">
-        <form class="regform1" action="update_profilestf.php" method="post">
-        <br>
-        <br>
-        <div class="form-row">
-            <label for="name">Name:</label>
-            <input type="text" name="name" value="<?php echo $name; ?>" required readOnly>
-        </div>
-
-
-        <div class="form-row">
-            <label for="email">Email:</label>
-            <input type="email" name="email" value="<?php echo $email; ?>" required readOnly>
-        </div>
-        
-        <div class="form-row">
-            <label for="mob">Mobile:</label>
-            <input type="text" name="mob" value="<?php echo $mob; ?>" required readOnly > 
-        </div>
-        
-        <div class="form-row">
-            <label for="dob">Date of Birth:</label>
-            <input type="date" name="dob" value="<?php echo $dob; ?>" required readOnly>
-        </div>
-        
-        <div class="form-row">
-            <label for="address">Address:</label>
-            <input type="text" name="address" value="<?php echo $address; ?>" required readOnly>
-        </div>
-        <br><br>
-        <button id="editBtn" style="color:white; margin-left: 45%; background-color: rgb(33, 33, 74); width: 15%; ">Edit</button>
-        <button id="saveBtn" style="display: none; color:white; margin-left: 45%; background-color: rgb(33, 33, 74); width: 15%;  ">Save</button>
-        
-        </form>  
+            <form id="profileForm" action="update_profilestf.php" method="post" enctype="multipart/form-data">
+                <br>
+                <br>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Name:</th>
+                        <td>
+                            <input type="text" name="name" value="<?php echo $name; ?>" required readonly>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Email:</th>
+                        <td>
+                            <input type="email" name="email" value="<?php echo $email; ?>" required readonly>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Mobile:</th>
+                        <td>
+                            <input type="text" name="mob" value="<?php echo $mob; ?>" required readonly>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Date of Birth:</th>
+                        <td>
+                            <input type="date" name="dob" value="<?php echo $dob; ?>" required readonly>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Address:</th>
+                        <td>
+                            <input type="text" name="address" value="<?php echo $address; ?>" required readonly>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Photo:</th>
+                        <td>
+                            <input type="file" name="image" accept="image/*">
+                            <?php if (!empty($image1)) { ?>
+                                <br>
+                                <img src="<?php echo $image1; ?>" style="height: 5%; width: 5%;">
+                            <?php } ?>
+                        </td>
+                    </tr>
+                </table>         
+                <br><br>
+                <button id="editBtn" type="button" style="color:white; margin-left: 45%; background-color: rgb(33, 33, 74); width: 15%;">Edit</button>
+                <button id="saveBtn" type="submit" style="display: none; color:white; margin-left: 45%; background-color: rgb(33, 33, 74); width: 15%;">Save</button>
+            </form>  
         </div>
     </div>
     <script>
@@ -131,7 +152,7 @@ $conn->close();
     document.getElementById("editBtn").addEventListener("click", function(event) {
         event.preventDefault(); // Prevent form submission
 
-        var form = document.querySelector(".regform1");
+        var form = document.getElementById("profileForm");
         var fields = form.querySelectorAll("input, select");
         for (var i = 0; i < fields.length; i++) {
             fields[i].readOnly = false;
