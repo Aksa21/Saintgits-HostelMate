ee159db8fb045245c871809dc321947cc7ffca25
Anjali Mathew <anjalimathew468@gmail.com>
2023-06-07 08:24:32 +0530

    after photo

--- stdsignup.php
+++ stdsignup.php
@@ -1,6 +1,15 @@
 <?php
+$uploadDir = 'C:\xampp\htdocs\mini\uploads/';
+
+if (!is_dir($uploadDir)) {
+    // Directory doesn't exist, create it
+    mkdir($uploadDir, 0777, true);
+} elseif (!is_writable($uploadDir)) {
+    // Directory exists, but doesn't have write permissions
+    die("Upload directory is not writable. Please set appropriate permissions.");
+}
+
 // Retrieve the form data from the frontend
-//$stf_id = $_POST['stf_id'];
 if (isset($_POST['name']) && isset($_POST['room_no']) && isset($_POST['admission_no']) && isset($_POST['program']) && isset($_POST['branch']) && isset($_POST['guardian']) && isset($_POST['guardian_mob']) && isset($_POST['address']) && isset($_POST['mob']) && isset($_POST['dob']) && isset($_POST['pin']) && isset($_POST['email']) && isset($_POST['food_type']) && isset($_POST['password'])) {
 $name = $_POST['name'];
 $room_no = $_POST['room_no'];
@@ -16,6 +25,27 @@ $pin = $_POST['pin'];
 $email = $_POST['email'];
 $food_type = $_POST['food_type'];
 $password=$_POST['password'];
+
+// Check if an image file was uploaded
+if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+    $image_name = $_FILES['image']['name'];
+    $image_tmp = $_FILES['image']['tmp_name'];
+    $image_size = $_FILES['image']['size'];
+    $image_type = $_FILES['image']['type'];
+
+    // Specify the directory to which the image will be uploaded
+    $image_directory = "uploads/";
+
+    // Generate a unique filename for the uploaded image
+    $image_path = $image_directory . uniqid() . '_' . $image_name;
+
+    // Move the uploaded image to the specified directory
+    move_uploaded_file($image_tmp, $image_path);
+} else {
+    // Default image path if no image was uploaded
+    $image_path = "";
+}
+
 }
 // Create a connection
 $conn = new mysqli('localhost', 'root', '', 'hostel');
@@ -25,8 +55,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else{
-    $stmt = $conn->prepare("INSERT INTO std_details(name, room_no, admission_no, program, branch, guardian, guardian_mob, address, mob, dob, pin, email, food_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sisssssssssss", $name,$room_no,$admission_no,$program,$branch,$guardian,$guardian_mob,$address,$mob,$dob,$pin,$email,$food_type);
+    $stmt = $conn->prepare("INSERT INTO std_details(name, room_no, admission_no, program, branch, guardian, guardian_mob, address, mob, dob, pin, email, food_type, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sissssssssssss", $name,$room_no,$admission_no,$program,$branch,$guardian,$guardian_mob,$address,$mob,$dob,$pin,$email,$food_type,$image_path);
     $stmt->execute();
     $std_id=$stmt->insert_id;
     $stmt->close();
